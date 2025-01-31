816192529c51a6b3f99e371702fd891985cbfdd6
RAMYA <RAMYA@Ramya>
2025-01-31 12:38:13 +0530

    first

--- Encapsulation/src/com/tnfif/constructor/Constructordemo.java
+++ Encapsulation/src/com/tnfif/constructor/Constructordemo.java
@@ -1,5 +1,21 @@
 package com.tnfif.constructor;
 import java.util.Scanner;
 public class Constructordemo {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the customer name");
+	String name=sc.next();
+	System.out.println("enter the customer id");
+	int id=sc.nextInt();
+	System.out.println("enter the customer address");
+	String address=sc.next();
+	Customer c=new Customer();
+	c.setCustomername(name);
+	c.setCustomerid(id);
+	c.setCustomeraddress(address);
+	System.out.println(c);
+	
+
+}
 
 }
--- Encapsulation/src/com/tnfif/constructor/Customer.java
+++ Encapsulation/src/com/tnfif/constructor/Customer.java
@@ -9,4 +9,34 @@ public class Customer {
 	 System.out.println("default");
   }
  //parameterized constructor
+	public Customer(String customername, int customerid, String customeraddress) {
+	
+		this.customername = customername;
+		this.customerid = customerid;
+		this.customeraddress = customeraddress;//use source after generate source of fields
+	}
+ //getter and setter
+	public String getCustomername() {
+		return customername;
+	}
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+	public int getCustomerid() {
+		return customerid;
+	}
+	public void setCustomerid(int customerid) {
+		this.customerid = customerid;
+	}
+	public String getCustomeraddress() {
+		return customeraddress;
+	}
+	public void setCustomeraddress(String customeraddress) {
+		this.customeraddress = customeraddress;
+	}
+	@Override//by string to string
+	public String toString() {
+		return "Customer [customername=" + customername + ", customerid=" + customerid + ", customeraddress="
+				+ customeraddress + "]";
+	}
 }
